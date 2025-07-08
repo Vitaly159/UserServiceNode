a338084e542816ad5e1fde5969f7b2c1da591c9e
Виталий <100439661+Vitaly159@users.noreply.github.com>
2025-07-09 02:13:06 +0500

    swagger

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import yaml from "js-yaml";
 import dotenv from "dotenv";
 dotenv.config();
-
 const app = express();
 
 app.use(express.json()); // для парсинга JSON
 
-// Здесь можно подключить маршруты, например:
-// import userRoutes from './routes/userRoutes';
-// app.use('/api/users', userRoutes);
+const swaggerDocument = yaml.load(fs.readFileSync("./swagger.yaml", "utf8")); // Загрузка спецификации
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument as swaggerUi.JsonObject)); // Маршрут для документации
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import app from "./app";
+import open from "open";
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
+  // open(`http://localhost:${PORT}/api-docs`);
 });
