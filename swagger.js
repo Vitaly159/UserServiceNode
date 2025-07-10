588cfffed9f4a22a2917a211147201e68aa20d84
Виталий <100439661+Vitaly159@users.noreply.github.com>
2025-07-11 00:39:19 +0500

    авто свагер

--- swagger.js
+++ swagger.js
@@ -1,17 +1,21 @@
+import fs from 'fs';
+import yaml from 'js-yaml';
 import swaggerJsdoc from 'swagger-jsdoc';
 
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'API',
-            version: '1.0.0',
-        },
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API',
+      version: '1.0.0',
     },
-    apis: ['./src/controllers/*.ts'], // укажите путь к вашим контроллерам с комментариями
+  },
+  apis: ['./src/controllers/*.ts'], // путь к вашим файлам с комментариями
 };
 
 const swaggerSpec = swaggerJsdoc(options);
 
-// экспортируйте или сохраняйте в файл
-export default swaggerSpec;
+// Записываем в файл
+fs.writeFileSync('./swagger.yaml', yaml.dump(swaggerSpec));
+
+console.log('Swagger YAML успешно сгенерирован');
