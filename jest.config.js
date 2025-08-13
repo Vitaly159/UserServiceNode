da483af938a7b5e90fa90b9ba9ee5ec35e530b03
Виталий <100439661+Vitaly159@users.noreply.github.com>
2025-08-13 21:58:55 +0500

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,2 +1,5 @@
 export const preset = 'ts-jest';
 export const testEnvironment = 'node';
+export const roots = ['<rootDir>/src'];
+export const moduleFileExtensions = ['ts', 'js'];
+export const testMatch = ['**/?(*.)+(spec|test).ts'];
